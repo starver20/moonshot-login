6cffae7fb2288255e40190723497e62a90e78bb3
starver20 <amarnarute2002@gmail.com>
2024-04-06 20:57:13 +0530

    sd

--- src/server/db.ts
+++ src/server/db.ts
@@ -1,16 +1,11 @@
-import { Client } from "@planetscale/database";
-import { PrismaPlanetScale } from "@prisma/adapter-planetscale";
 import { PrismaClient } from "@prisma/client";
 
 import { env } from "~/env";
-
-const psClient = new Client({ url: env.DATABASE_URL });
 
 const createPrismaClient = () =>
   new PrismaClient({
     log:
       env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
-    adapter: new PrismaPlanetScale(psClient),
   });
 
 const globalForPrisma = globalThis as unknown as {
@@ -18,5 +13,4 @@ const globalForPrisma = globalThis as unknown as {
 };
 
 export const db = globalForPrisma.prisma ?? createPrismaClient();
-
 if (env.NODE_ENV !== "production") globalForPrisma.prisma = db;
